7bdc1a79c5d7827ef051f4e1f2b765e64092c749
nvduarte <95474953+nvduarte@users.noreply.github.com>
2024-05-10 16:12:24 +0200

    Add diagonal pixels to cluster

--- clustering1D.py
+++ clustering1D.py
@@ -2,11 +2,12 @@ import numpy as np
 
 def clusterize(data_in,
                noise_map,
-               cluster_dim=(3,3),
                N_sigma_high=5,
                N_sigma_low=3,
-               allow_2D=False,
+               allow_2D=True,
+               allow_diag=True,
                E_threshold=0,
+               cluster_dim=(3,3),
                direction='vertical'):
     
     """
@@ -16,14 +17,16 @@ def clusterize(data_in,
     Args:
         data_in (numpy.ndarray): Input data representing the x-ray charge integrating pixel detector.
         noise_map (numpy.ndarray): Noise map.
-        cluster_dim (tuple): Dimension of the window used to find clusters. Must be a pair of odd numbers. Defaults to (3,3).
         N_sigma_high (int): Multiplier of noise above which pixels may be considered as  
                             the center of a cluster. Defaults to 5.
         N_sigma_low (int): Multiplier of noise above which pixels are considered to contain 
                            charge from a photon hit. Defaults to 3.
         allow_2D (bool): Flag to allow 2D clustering. If True, the highest neighbour of the central pixel 
                          on the non-dominant charge sharing direction may be added to the cluster. Defaults to False.
-        E_threshold (int): Energy threshold below which found clusters are discarded. Defaults to 0.
+        allow_diag (bool): Flag to allow clustering of diagonal pixels. If True, the highest vertical neighbour of the
+                           previously determined horizontal neighbour may be added to the cluster. Defaults to False  
+        E_threshold (float): Energy threshold below which found clusters are discarded. Defaults to 0.
+        cluster_dim (tuple): Dimension of the window used to find clusters. Must be a pair of odd numbers. Defaults to (3,3).
         direction (str): Dominant charge sharing direction, either 'horizontal' or 'vertical'. Defaults to 'vertical'.
 
     Returns:
@@ -69,19 +72,20 @@ def clusterize(data_in,
     clu = data_in.copy()
     cluster_size = np.zeros(data_in.shape, dtype=np.int8)
 
-    
     # 0. Loop over trains and memory cells
     for train in range(trains):
         for mc in range(memory_cells):
         
-            # 1. Find photon clusters
+            # 1. Find photon clusters per frame
             photon_hits = np.transpose(np.where(data_in[train, mc]>N_sigma_high*noise_map[mc])).tolist()
+            
             for phy, phx in photon_hits:
                 # discard photon hits on sensor borders
                 if (center_y-1 < phy < pixels_y-center_y) and (center_x-1 < phx < pixels_x-center_x):
                     
                     cluster = data_in[train, mc][phy-center_y:phy+center_y+1, phx-center_x:phx+center_x+1]
                     cluster_noise = noise_map[mc][phy-center_y:phy+center_y+1, phx-center_x:phx+center_x+1]
+                    integrated_noise = 0
 
                     # 2. Check if index is center of cluster (max of direct horizontal/vertical neighbours)
                     if (cluster*cross_mask).argmax() == center_idx:
@@ -89,49 +93,110 @@ def clusterize(data_in,
                         # 3. Sum pixels in cluster
                         # start with cluster center
                         cluster_sum = cluster[center_y,center_x]
-
+                        integrated_noise += np.sqrt(cluster_noise[center_y,center_x])
+                        
                         # look for vertical neighbours above N_low_sigma * noise
                         neighbours_down, neighbours_up, neighbours_left, neighbours_right = 0, 0, 0, 0
+                        neighbours_left_down, neighbours_left_up, neighbours_right_down, neighbours_right_up = 0,0,0,0
+
                         # while vertical neighbours downwards are above N_low_sigma, sum them
                         iy = center_y-1
                         while (iy > -1) and (cluster[iy,center_x] > cluster_noise[iy,center_x]*N_sigma_low):
                             cluster_sum += cluster[iy,center_x]
+                            integrated_noise += np.sqrt(cluster_noise[iy,center_x])
                             neighbours_down += 1
                             iy -= 1
+                            
                         # while vertical neighbours upwards are above N_low_sigma, sum them
                         iy = center_y+1
                         while (iy < cluster_dim[0]) and (cluster[iy,center_x] > cluster_noise[iy,center_x]*N_sigma_low):
                             cluster_sum += cluster[iy,center_x]
+                            integrated_noise += np.sqrt(cluster_noise[iy,center_x])
                             neighbours_up += 1
                             iy += 1
 
                         # optionally, check if there are horizontal neighbours above N_low_sigma * noise. 
                         # If yes sum the highest one, except if it belongs to another horizontal cluster.
                         if allow_2D:
-                            if (cluster[center_y,center_x-1]>cluster[center_y,center_x+1]) and (np.argmax(cluster[center_y-1:center_y+2, center_x-1])==1):
-                                    
+                            # left neighbour
+                            if (cluster[center_y,center_x-1]>cluster[center_y,center_x+1] and
+                                cluster[center_y,center_x-1]>cluster_noise[center_y,center_x-1]*N_sigma_low and
+                                np.argmax(cluster[center_y-1:center_y+2, center_x-1])==1):
                                 cluster_sum += cluster[center_y,center_x-1]
+                                integrated_noise += np.sqrt(cluster_noise[center_y,center_x-1])
                                 neighbours_left = 1
                                 
-                            elif (cluster[center_y,center_x+1]>cluster[center_y,center_x-1]) and (np.argmax(cluster[center_y-1:center_y+2, center_x+1])==1):
+                                if allow_diag:
+                                    if (neighbours_down > neighbours_up and # make sure 4 px events are square shaped
+                                        cluster[center_y-1,center_x-1]<cluster[center_y,center_x-1] and # TODO: min of all 2x2 
+                                        cluster[center_y-1,center_x-1]>cluster_noise[center_y-1,center_x-1]*N_sigma_low): 
+                                        cluster_sum += cluster[center_y-1,center_x-1]
+                                        integrated_noise += np.sqrt(cluster_noise[center_y-1,center_x-1])
+                                        neighbours_left_down = 1
+                                    elif (neighbours_up > neighbours_down and
+                                          cluster[center_y+1,center_x-1]<cluster[center_y,center_x-1] and
+                                          cluster[center_y+1,center_x-1]>cluster_noise[center_y+1,center_x-1]*N_sigma_low):
+                                        integrated_noise += np.sqrt(cluster[center_y+1,center_x-1])
+                                        neighbours_left_up = 1
+
+                            # right neighbour
+                            elif (cluster[center_y,center_x+1]>cluster[center_y,center_x-1] and
+                                  cluster[center_y,center_x+1]>cluster_noise[center_y,center_x+1]*N_sigma_low and
+                                  np.argmax(cluster[center_y-1:center_y+2, center_x+1])==1):
                                 cluster_sum += cluster[center_y,center_x+1]
+                                integrated_noise += np.sqrt(cluster_noise[center_y,center_x+1])
                                 neighbours_right = 1
-                            
+                                
+                                if allow_diag:
+                                    if (neighbours_down > neighbours_up and 
+                                        cluster[center_y-1,center_x+1]<cluster_noise[center_y,center_x+1] and
+                                        cluster[center_y-1,center_x+1]>cluster_noise[center_y-1,center_x+1]*N_sigma_low):
+                                        cluster_sum += cluster[center_y-1,center_x+1]
+                                        integrated_noise += np.sqrt(cluster_noise[center_y-1,center_x+1])
+                                        neighbours_right_down = 1
+                                    elif (neighbours_up > neighbours_down and
+                                          cluster[center_y+1,center_x+1]<cluster[center_y,center_x+1] and
+                                          cluster[center_y+1,center_x+1]>cluster_noise[center_y+1,center_x+1]*N_sigma_low):
+                                        cluster_sum += cluster[center_y+1,center_x+1]
+                                        integrated_noise += np.sqrt(cluster_noise[center_y+1,center_x+1])
+                                        neighbours_right_up = 1
+                                        
                         # 4. Pass cluster sum to center pixel
                         if cluster_sum > E_threshold:
                             
+                            # Calculate cluster size (number of pixels in cluster)
+                            neighbours = 1+neighbours_down+neighbours_up+neighbours_left+neighbours_right
+                            neighbours += neighbours_left_down+neighbours_left_up+neighbours_right_down+neighbours_right_up
+
+                            # Fill cluster_size array
+                            cluster_size[train, mc, phy-neighbours_down:phy, phx] = -neighbours # vertical down
+                            cluster_size[train, mc, phy:phy+neighbours_up+1, phx] = -neighbours # vertical up
+                            cluster_size[train, mc, phy, phx-neighbours_left] = -neighbours     # horizontal left
+                            cluster_size[train, mc, phy, phx+neighbours_right] = -neighbours    # horizontal right
+                            cluster_size[train, mc, phy+neighbours_right_up, phx+neighbours_right] = -neighbours # Up Right
+                            cluster_size[train, mc, phy-neighbours_right_down, phx+neighbours_right_down] = -neighbours # DR
+                            cluster_size[train, mc, phy-neighbours_left_down, phx-neighbours_left_down] = -neighbours # DL
+                            cluster_size[train, mc, phy+neighbours_left_up, phx-neighbours_left_up] = -neighbours # UL
+                            cluster_size[train, mc, phy, phx] = neighbours
+                            
+                            # Fill clu array
                             clu[train, mc,
                                 phy-neighbours_down:phy+neighbours_up+1,
                                 phx-neighbours_left:phx+neighbours_right+1] = 0 # set neighbours to zero 
                             
-                            clu[train, mc, phy, phx] = cluster_sum
+                            # Attribute total energy to central pixel
+                            clu[train, mc, phy, phx] = cluster_sum - (integrated_noise/neighbours)*(np.sqrt(neighbours)-1)
                             
-                            neighbours = 1+neighbours_down+neighbours_up+neighbours_left+neighbours_right
-                            cluster_size[train, mc, phy-neighbours_down:phy, phx] = -neighbours
-                            cluster_size[train, mc, phy:phy+neighbours_up+1, phx] = -neighbours
-                            cluster_size[train, mc, phy, phx-neighbours_left] = -neighbours
-                            cluster_size[train, mc, phy, phx+neighbours_right] = -neighbours
-                            cluster_size[train, mc, phy, phx] = neighbours
+                            # Zero-out already clusterized pixels
+                            data_in[train, mc, phy-neighbours_down:phy, phx] = 0 # vertical down
+                            data_in[train, mc, phy:phy+neighbours_up+1, phx] = 0 # vertical up
+                            data_in[train, mc, phy, phx-neighbours_left] = 0     # horizontal left
+                            data_in[train, mc, phy, phx+neighbours_right] = 0    # horizontal right
+                            data_in[train, mc, phy+neighbours_right_up, phx+neighbours_right] = 0 # Up Right
+                            data_in[train, mc, phy-neighbours_right_down, phx+neighbours_right_down] = 0 # D R
+                            data_in[train, mc, phy-neighbours_left_down, phx-neighbours_left_down] = 0 # D L
+                            data_in[train, mc, phy+neighbours_left_up, phx-neighbours_left_up] = 0 # U L
+                            data_in[train, mc, phy, phx] = 0                                
 
     # restore data_in shape if it was changed 
     if direction.lower()=='horizontal':
@@ -142,3 +207,7 @@ def clusterize(data_in,
         cluster_size = cluster_size.squeeze()
 
     return clu, cluster_size
+
+# TODOs: 
+# [x] set data_in to zero after being added to cluster to prevent being added to more than 1 cluster
+# [ ] Only assumes cluster size of (3, 3) - line 86: cluster = data_in[train, mc][phy-center_y:phy+center_y+1, phx-center_x:phx+center_x+1]
